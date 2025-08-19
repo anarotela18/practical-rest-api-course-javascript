4d2e0abd8d40adcc883dc45cb00c53294f30dccb
Ana Rotela <anarotela18@gmail.com>
2025-08-19 10:57:29 -0300

    Migrate project to Axios

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
+const api = axios.create({
+  baseURL: 'https://api.themoviedb.org/3/',
+  headers: {
+    'Content-Type': 'application/json;charset=utf-8',
+  },  
+  params: {
+    'api_key': API_KEY
+  }
+});
 const IMG_PREVIEW = 'http://image.tmdb.org/t/p/w300';
 
 async function getTrendingMoviesPreview(){
-  const res = await fetch('https://api.themoviedb.org/3/trending/movie/day?api_key='+API_KEY);
-  const data = await res.json();
+  const {data} = await api('trending/movie/day');
   const movies = data.results;
   movies.forEach(movie =>{
     const trendingPreviewMoviesContainer = document.querySelector("#trendingPreview .trendingPreview-movieList");
@@ -19,8 +27,7 @@ async function getTrendingMoviesPreview(){
   });
 };
 async function getCategoriesMoviesPreview(){
-  const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key='+API_KEY);
-  const data = await res.json();
+  const {data} = await api('genre/movie/list');
   const categories = data.genres;
   categories.forEach(category => {
     const categoryContainer = document.querySelector("#categoriesPreview .categoriesPreview-list");

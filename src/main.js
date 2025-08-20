8a762cb8cffaf488b8fbc85b60ee6671ce468d97
Ana Rotela <anarotela18@gmail.com>
2025-08-19 21:56:00 -0300

    Filter movies by categories

--- src/main.js
+++ src/main.js
@@ -12,8 +12,10 @@ const IMG_PREVIEW = 'http://image.tmdb.org/t/p/w300';
 async function getTrendingMoviesPreview(){
   const {data} = await api('trending/movie/day');
   const movies = data.results;
+
+  trendingMoviesPreviewList.innerHTML = "";
+
   movies.forEach(movie =>{
-    const trendingPreviewMoviesContainer = document.querySelector("#trendingPreview .trendingPreview-movieList");
     const movieContainer = document.createElement("div");
     movieContainer.classList.add("movie-container");
 
@@ -23,26 +25,54 @@ async function getTrendingMoviesPreview(){
     movieImg.src = IMG_PREVIEW + movie.poster_path;
 
     movieContainer.appendChild(movieImg);
-    trendingPreviewMoviesContainer.appendChild(movieContainer);
+    trendingMoviesPreviewList.appendChild(movieContainer);
   });
 };
 async function getCategoriesMoviesPreview(){
   const {data} = await api('genre/movie/list');
   const categories = data.genres;
+
+  categoriesPreviewList.innerHTML = "";
+
   categories.forEach(category => {
-    const categoryContainer = document.querySelector("#categoriesPreview .categoriesPreview-list");
-    
     const divCategoryContainer = document.createElement("div");
     divCategoryContainer.classList.add("category-container");
 
     const categoryTitle = document.createElement("h3");
     categoryTitle.classList.add("category-title");
     categoryTitle.setAttribute("id","id"+category.id);
+    categoryTitle.addEventListener('click',()=>{
+      location.hash =`#category=${category.id}-${category.name}`;
+    });
 
     const categoryTitleText = document.createTextNode(category.name);
     
     categoryTitle.appendChild(categoryTitleText);
     divCategoryContainer.appendChild(categoryTitle);
-    categoryContainer.appendChild(divCategoryContainer);
+    categoriesPreviewList.appendChild(divCategoryContainer);
   }); 
+}
+async function getMoviesByCategory(id){
+  console.log('getmovies');
+  const {data} = await api('discover/movie',{
+    params: {
+      with_genres: id
+    }
+  });
+  const categories = data.results;
+
+  genericSection.innerHTML = "";
+
+  categories.forEach(category => {
+    const categoryContainer = document.createElement("div");
+    categoryContainer.classList.add('movie-container');
+    
+    const categoryImg = document.createElement("img");
+    categoryImg.classList.add('movie-img');
+    categoryImg.alt = category.title;
+    categoryImg.src = IMG_PREVIEW + category.poster_path;
+    
+    categoryContainer.appendChild(categoryImg);
+    genericSection.appendChild(categoryContainer);
+  });
 }
--- src/navigation.js
+++ src/navigation.js
@@ -1,3 +1,14 @@
+searchFormBtn.addEventListener('click',()=>{
+  location.hash="#search=";
+});
+trendingBtn.addEventListener('click',()=>{
+  location.hash="#trends";
+});
+arrowBtn.addEventListener('click',()=>{
+  location.hash="#home";
+});
+
+
 window.addEventListener('DOMContentLoaded',navigator,false);
 window.addEventListener('hashchange',navigator,false);
 
@@ -21,19 +32,91 @@ function navigator(){
   }
 }
 function homePage(){
-  console.log("HOME");
+  console.log("HOME!!");
+
+  headerSection.classList.remove("header-container--long");
+  headerSection.style.background = "";
+  arrowBtn.classList.add("inactive");
+  arrowBtn.classList.remove('header-arrow--white');
+  headerTitle.classList.remove("inactive");
+  headerCategoryTitle.classList.add("inactive");
+  searchForm.classList.remove("inactive");
+
+  trendingPreviewSection.classList.remove("inactive");
+  categoriesPreviewSection.classList.remove("inactive");
+  genericSection.classList.add("inactive");
+  movieDetailSection.classList.add("inactive");
+
   getTrendingMoviesPreview();
   getCategoriesMoviesPreview();
 }
 function categoriesPage(){
   console.log("CATEGORIES!!");
+
+  headerSection.classList.remove("header-container--long");
+  headerSection.style.background = "";
+  arrowBtn.classList.remove("inactive");
+  arrowBtn.classList.remove("header-arrow--white");
+  headerTitle.classList.add("inactive");
+  headerCategoryTitle.classList.remove("inactive");
+  searchForm.classList.add("inactive");
+
+  trendingPreviewSection.classList.add("inactive");
+  categoriesPreviewSection.classList.add("inactive");
+  genericSection.classList.remove("inactive");
+  movieDetailSection.classList.add("inactive");
+
+  const [_,categoryData] = location.hash.split('=') // ["#category","id-name"];
+  const [categoryId, categoryName] = categoryData.split('-');
+  
+  getMoviesByCategory(categoryId);
 }
 function movieDetailsPage(){
   console.log("MOVIE!!");
+
+  headerSection.classList.add('header-container--long');
+  arrowBtn.classList.remove('inactive');
+  arrowBtn.classList.add('header-arrow--white');
+  headerTitle.classList.add('inactive');
+  headerCategoryTitle.classList.add('inactive');
+  searchForm.classList.add('inactive');
+
+  trendingPreviewSection.classList.add('inactive');
+  categoriesPreviewSection.classList.add('inactive');
+  genericSection.classList.add('inactive');
+  movieDetailSection.classList.remove('inactive');
 }
 function searchPage(){
   console.log("SEARCH!!");
+
+  headerSection.classList.remove("header-container--long");
+  headerSection.style.background = "";
+  arrowBtn.classList.remove("inactive");
+  arrowBtn.classList.remove("header-arrow--white");
+  headerTitle.classList.add("inactive");
+  headerCategoryTitle.classList.remove("inactive");
+  searchForm.classList.remove("inactive");
+
+  trendingPreviewSection.classList.add("inactive");
+  categoriesPreviewSection.classList.add("inactive");
+  genericSection.classList.remove("inactive");
+  movieDetailSection.classList.add("inactive");
+
 }
 function trendsPage(){
   console.log("TRENDS!!");
+
+  headerSection.classList.remove("header-container--long");
+  headerSection.style.background = "";
+  arrowBtn.classList.remove("inactive");
+  arrowBtn.classList.remove("header-arrow--white");
+  headerTitle.classList.add("inactive");
+  headerCategoryTitle.classList.remove("inactive");
+  searchForm.classList.add("inactive");
+
+  trendingPreviewSection.classList.add("inactive");
+  categoriesPreviewSection.classList.add("inactive");
+  genericSection.classList.remove("inactive");
+  movieDetailSection.classList.add("inactive");
+
 }

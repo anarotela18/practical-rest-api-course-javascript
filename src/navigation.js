dfc45936bed7d809ff140ada7554323df7019fe0
Ana Rotela <anarotela18@gmail.com>
2025-08-22 10:53:15 -0300

    Movie details implemented

--- src/navigation.js
+++ src/navigation.js
@@ -90,6 +90,10 @@ function movieDetailsPage(){
   categoriesPreviewSection.classList.add('inactive');
   genericSection.classList.add('inactive');
   movieDetailSection.classList.remove('inactive');
+
+  // ['#movie','234567']
+  const [_,movieId] = location.hash.split('=');
+  getMovieById(movieId);
 }
 function searchPage(){
   console.log("SEARCH!!");
@@ -128,6 +132,6 @@ function trendsPage(){
   movieDetailSection.classList.add("inactive");
 
   headerCategoryTitle.innerHTML = "Tendencias";
-  getMoviesPreview();
+  getTrendingMovies();
 
 }
